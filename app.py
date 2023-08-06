006fd4d50c44eda145c526428ac14616879d67fd
Max <maxlys2001@gmail.com>
2023-08-06 13:47:44 +0300

    Remove unnecessary page, Remove post request option.

--- app.py
+++ app.py
@@ -5,22 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-@app.route("/generate", methods=["GET", "POST"])
-def generate():
-    if request.method == "POST":
-        # Generate the bravery build
-        return redirect("/")
-    else:
-        if request.args.get("level") == "1":
-            # Easiest level
-            return render_template("generate.html", level=1)
-        elif request.args.get("level") == "2":
-            # Medium level
-            return render_template("generate.html", level=2)
-        elif request.args.get("level") == "3":
-            # Hardest level
-            return render_template("generate.html", level=3)
-        else:
-            # Invalid level
-            return redirect("/")
